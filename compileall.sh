2dcbcd2988065078a56acbe95154bee0f5b762e3
saeed0x1 <behesti4053@gmail.com>
2024-01-09 10:23:52 +0530

    Updated compileall.sh

--- compileall.sh
+++ compileall.sh
@@ -5,3 +5,4 @@ cp helloworld.py ./compiled/
 crystal build helloworld.cr -o ./compiled/helloworldcr
 go build -o ./compiled/helloworldgo ./helloworld.go
 g++ ./helloworld.cpp -o ./compiled/helloworldcpp
+gcc ./helloworld.c -o ./compiled/helloworldc
